419237ffadb7fbc865c511e4f5ccbda0582aaba6
James Cook <mokus@deepbondi.net>
2015-08-21 10:40:44 -0700

    polykinds!

--- src/Data/Dependent/Map/Internal.hs
+++ src/Data/Dependent/Map/Internal.hs
@@ -5,6 +5,9 @@
 #if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ >= 702
 {-# LANGUAGE Safe #-}
 #endif
+#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ >= 708
+{-# LANGUAGE PolyKinds #-}
+#endif
 module Data.Dependent.Map.Internal where
 
 import Data.Dependent.Sum
